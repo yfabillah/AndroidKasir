0cd849410acc624f0da7877eba1f4de633d79378
Yudith Fabillah <yudith@upi.edu>
2022-01-03 10:46:38 +0700

    first commit

---
+++ app/src/main/java/com/example/androidkasir/User.java
@@ -0,0 +1,15 @@
+package com.example.androidkasir;
+
+public class User {
+
+    public String name, email;
+
+    public User(){
+
+    }
+
+    public User(String name, String username){
+        this.name = name;
+        this.email = email;
+    }
+}
